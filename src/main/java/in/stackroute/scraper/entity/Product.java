5e04e8596c78eaa5ee656954bcf434fe1edcf353
Ajinkya Bawaskar <ajinkyabawaskar2@gmail.com>
2021-02-22 10:32:16 +0530

    removed @entity from Product model

--- src/main/java/in/stackroute/scraper/entity/Product.java
+++ src/main/java/in/stackroute/scraper/entity/Product.java
@@ -1,18 +1,14 @@
 package in.stackroute.scraper.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
 @Getter
 @Setter
 @ToString
 public class Product implements Comparable<Product> {
-	@Id
+
 	private Long pid;
 	private String name;
 	private String ratings;
